109454320b5e82523b2431e1c6e549da9747a603
bhimasantosh <santoshbhima@outlook.com>
2014-06-17 00:15:17 +0530

    Pending  Changes
    changed the path of spring context xml in web.xml

---
+++ src/main/java/com/mbits/auth/controller/SampleController.java
@@ -0,0 +1,19 @@
+package com.mbits.auth.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleController {
+
+	@ResponseBody
+	@RequestMapping(value = "getData")
+	public String getResponse(HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse) {
+		return "Sample Response";
+	}
+}
